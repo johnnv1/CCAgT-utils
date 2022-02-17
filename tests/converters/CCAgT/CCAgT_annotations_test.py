2df7145b97b29d991a6777267da551a9a978fcf2
João Gustavo Atkinson Amorim <joaogustavoamorim@gmail.com>
2022-02-17 15:56:20 -0300

    add test for get slide id and tests TODO

--- tests/converters/CCAgT/CCAgT_annotations_test.py
+++ tests/converters/CCAgT/CCAgT_annotations_test.py
@@ -31,3 +31,20 @@ def test_init_wrong_columns():
 def test_init_wrong_data():
     with pytest.raises(TypeError):
         CCAgT_Annotations([{'image_name': 'A', 'geometry': Point(1, 1)}])
+
+
+def test_get_slide_id():
+    df = pd.DataFrame([{'image_name': 'A_xxx', 'geometry': Point(1, 1), 'category_id': 1}])
+    ccagt_ann = CCAgT_Annotations(df)
+
+    assert ccagt_ann.get_slide_id().to_numpy().tolist() == ['A']
+
+# TODO: test for CCAgT_Annotations.satellite_point_to_polygon
+# TODO: test for CCAgT_Annotations.fit_geometries_to_image_boundary
+# TODO: test for CCAgT_Annotations.geometries_type
+# TODO: test for CCAgT_Annotations.geometries_area
+# TODO: test for CCAgT_Annotations.generate_ids
+# TODO: test for CCAgT_Annotations.delete_by_area
+# TODO: test for CCAgT_Annotations.find_intersecting_geometries
+# TODO: test for CCAgT_Annotations.find_overlapping_annotations
+# TODO: test for CCAgT_Annotations.union_geometries
