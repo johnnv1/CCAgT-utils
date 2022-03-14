406ac8964f3aec8f754d4699a3c3ada4bd62f898
João Gustavo Atkinson Amorim <joaogustavoamorim@gmail.com>
2022-03-14 19:35:00 -0300

    add describe image files with multiprocessing

--- CCAgT_utils/describe.py
+++ CCAgT_utils/describe.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import multiprocessing
+from dataclasses import dataclass
 from typing import Tuple
 from typing import Union
 
@@ -7,24 +9,64 @@ import numpy as np
 from PIL import Image
 
 from CCAgT_utils.utils import find_files
+from CCAgT_utils.utils import get_traceback
 
-R = Union[float, Tuple[float, ...]]
+R = Union[float, Tuple[float, float, float]]
 
 
-def array(array: np.ndarray) -> tuple[R, R, R, R]:
+@dataclass
+class Statistics:
+    mean: R = 0.
+    std: R = 0.
+    max: R = 0.
+    min: R = 0.
+    count: int = 0
+
+    def join_stats(self, results: Statistics) -> None:
+        if self.count == 0:
+            self.mean = results.mean
+            self.std = results.std
+            self.max = results.max
+            self.min = results.min
+        else:
+            self.mean = np.mean([self.mean, results.mean], axis=0)
+            self.std = np.mean([self.std, results.std], axis=0)
+            self.max = np.max([self.max, results.max], axis=0)
+            self.min = np.min([self.min, results.min], axis=0)
+        self.count += results.count
+
+
+def from_array(array: np.ndarray) -> Statistics:
     axis = (0, 1)
     _mean = np.mean(array, axis=axis)
     _std = np.std(array, axis=axis)
     _max = np.max(array, axis=axis)
     _min = np.min(array, axis=axis)
 
-    return (_mean, _std, _max, _min)
+    return Statistics(_mean, _std, _max, _min, count=1)
 
 
-def image_files(images_dir: str,
-                extensions: str | tuple[str, ...] = '.jpg',
-                selection: set[str] = set()
-                ) -> dict[str, R]:
+@get_traceback
+def single_core_from_image_files(filenames: list[str]) -> Statistics:
+    if len(filenames) == 0:
+        raise
+
+    out_stats = Statistics()
+    for filename in filenames:
+        out_stats.join_stats(
+            from_array(
+                np.asarray(
+                    Image.open(filename)
+                )
+            ))
+
+    return out_stats
+
+
+def from_image_files(images_dir: str,
+                     extensions: str | tuple[str, ...] = '.jpg',
+                     selection: set[str] = set()
+                     ) -> Statistics:
     """From a directory path with images, will generate the stats of all
     images. The statistics generated are: mean, std, max, and min.
 
@@ -49,21 +91,23 @@ def image_files(images_dir: str,
 
     all_images = find_files(images_dir, extensions, True, selection)
 
-    filenames = list(all_images.values())
+    all_filenames = list(all_images.values())
 
-    out = {}
-    _mean, _std, _max, _min = array(np.asarray(Image.open(filenames[0])))
-    out['mean'] = _mean
-    out['std'] = _std
-    out['max'] = _max
-    out['min'] = _min
+    cpu_num = multiprocessing.cpu_count()
+    workers = multiprocessing.Pool(processes=cpu_num)
 
-    for filename in filenames[1:]:
-        _mean, _std, _max, _min = array(np.asarray(Image.open(filename)))
+    filenames_splitted = np.array_split(all_filenames, cpu_num)
+    print(f'Start compute Statistics for {len(all_filenames)} ({extensions}) files using {cpu_num} cores with '
+          f'{len(filenames_splitted[0])} files per core...')
 
-        out['mean'] = np.mean([out['mean'], _mean], axis=0)
-        out['std'] = np.mean([out['std'], _std], axis=0)
-        out['max'] = np.max([out['max'], _max], axis=0)
-        out['min'] = np.min([out['min'], _min], axis=0)
+    processes = []
+    for filenames in filenames_splitted:
+        p = workers.apply_async(single_core_from_image_files, (filenames.tolist(), ))
+        processes.append(p)
 
-    return out
+    out_stats = Statistics()
+    for p in processes:
+        out_stats.join_stats(p.get())
+
+    print(f'Successfully computed the statstics of {out_stats.count} files!')
+    return out_stats
