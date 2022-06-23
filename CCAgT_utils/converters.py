f6be1a8d5bc2b01d3a172863530051fcaf1af585
João Gustavo Atkinson Amorim <joaogustavoamorim@gmail.com>
2022-06-23 14:32:45 -0300

    move generate masks

--- CCAgT_utils/converters.py
+++ CCAgT_utils/converters.py
@@ -1,14 +1,20 @@
 from __future__ import annotations
 
+import collections
 from typing import Any
 
 import numpy as np
 import pandas as pd
+from PIL import Image
+from PIL import ImageDraw
 from shapely.geometry import Point
 from shapely.geometry import Polygon
 
+from CCAgT_utils.base.categories import Categories
 from CCAgT_utils.base.categories import CategoriesInfos
 from CCAgT_utils.base.utils import basename
+from CCAgT_utils.formats.annotation import Annotation
+from CCAgT_utils.formats.mask import Mask
 
 
 # -----------------------------------------------------------------------
@@ -135,4 +141,54 @@ def from_labelbox(
 
 
 # -----------------------------------------------------------------------
-# Functions to
+# Functions to generate masks
+
+
+# Order of categories IDs available at the metadata
+DRAW_ORDER = (
+    Categories.LEUKOCYTE_NUCLEUS,
+    Categories.NON_VIABLE_NUCLEUS,
+    Categories.OVERLAPPED_NUCLEI,
+    Categories.NUCLEUS_OUT_OF_FOCUS,
+    Categories.NUCLEUS,
+    Categories.CLUSTER,
+    Categories.SATELLITE,
+)
+
+
+def order_annotations_to_draw(annotations: list[Annotation]) -> list[Annotation]:
+    items = collections.defaultdict(list)
+    for ann in annotations:
+        items[ann.category_id].append(ann)
+    out = [ann for cat in DRAW_ORDER for ann in items[cat.value]]
+
+    return out
+
+
+def annotations_to_mask(
+    annotations: list[Annotation],
+    width: int = 1600,
+    height: int = 1200,
+) -> Mask:
+    shape = (height, width)
+
+    annotations_sorted = order_annotations_to_draw(annotations)
+
+    out = Image.fromarray(np.zeros(shape, dtype=np.uint8))
+    for ann in annotations_sorted:
+        out = draw_annotation(out, ann, ann.category_id)
+
+    return Mask(np.array(out))
+
+
+def draw_annotation(
+    target: Image.Image,
+    annotation: Annotation,
+    value: int | tuple[int, int, int],
+) -> Image.Image:
+    for geo in annotation:
+        pol_x, pol_y = geo.exterior.coords.xy
+        coords = list(zip(pol_x, pol_y))
+        ImageDraw.Draw(target).polygon(coords, fill=value)
+
+    return target
--- tests/converters_test.py
+++ tests/converters_test.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
 
+import numpy as np
 import pandas as pd
 import pytest
 
+from CCAgT_utils.converters import annotations_to_mask
 from CCAgT_utils.converters import from_labelbox
 from CCAgT_utils.converters import lbox_geo_to_shapely
+from CCAgT_utils.converters import order_annotations_to_draw
+from CCAgT_utils.formats.annotation import Annotation
 
 
 def test_lbox_geo_to_shapely(lbox_raw_single_satellite, satellite_ex):
@@ -45,3 +49,17 @@ def test_from_labelbox_with_duplicated_image(
     lbox_df_raw = pd.DataFrame(lbox_raw_sample_complete)
     with pytest.raises(ValueError):
         from_labelbox(lbox_df_raw, categories_infos)
+
+
+def test_order_annotations_to_draw(cluster_ex, satellite_ex, nucleus_ex):
+    anns = [Annotation(cluster_ex, 2), Annotation(satellite_ex, 3), Annotation(nucleus_ex, 1)]
+    anns = order_annotations_to_draw(anns)
+
+    categories_ids = [ann.category_id for ann in anns]
+    assert categories_ids == [1, 2, 3]
+
+
+def test_annotations_to_mask(cluster_ex, cluster_mask_ex):
+    shape = cluster_mask_ex.shape
+    msk = annotations_to_mask([Annotation(cluster_ex, 2)], shape[0], shape[1])
+    assert np.array_equal(cluster_mask_ex, msk.categorical)
