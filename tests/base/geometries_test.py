29e6e513e02da019d620daf2bbe6f80affa69e50
João Gustavo Atkinson Amorim <joaogustavoamorim@gmail.com>
2022-06-23 11:04:27 -0300

    tests base

--- tests/base/geometries_test.py
+++ tests/base/geometries_test.py
@@ -2,7 +2,10 @@ from __future__ import annotations
 
 import numpy as np
 import pytest
+from shapely.geometry import box
+from shapely.geometry import Polygon
 
+from CCAgT_utils.base.geometries import clip_to_extent
 from CCAgT_utils.base.geometries import point_to_polygon
 
 
@@ -30,3 +33,22 @@ def test_point_to_polygon(ccagt_df_multi, satellite_ex, area_size, resolution, t
     diameter = np.sqrt(area_size / np.pi)
     pol_sat = satellite_ex.buffer(diameter, resolution).simplify(tolerance)
     assert sat_series_pol.to_numpy()[0].equals(pol_sat)
+
+
+@pytest.mark.parametrize(
+    'pol,bb,expected', [
+        (Polygon([(0, 0), (0, 10), (10, 10), (10, 15), (15, 15)]), box(0, 0, 10, 10), Polygon([(0, 0), (0, 10), (10, 10)])),
+        (Polygon([(0, 0), (0, 10), (10, 10), (10, 15), (15, 15)]), box(50, 50, 100, 100), np.nan),
+        (
+            Polygon([(0, 0), (0, 10), (10, 10), (10, 15), (15, 15)]), box(
+                0, 0, 100, 100,
+            ), Polygon([(0, 0), (0, 10), (10, 10), (10, 15), (15, 15)]),
+        ),
+    ],
+)
+def test_clip_to_extent(pol, bb, expected):
+    out = clip_to_extent(pol, bb)
+    if isinstance(expected, Polygon):
+        assert expected.equals(out)
+    else:
+        assert np.isnan(expected) and np.isnan(out)
