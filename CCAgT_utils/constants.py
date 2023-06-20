d2ff37bcb2d1a52debf67ca9f61fa434d6083535
João Gustavo A. Amorim <joaogustavoamorim@gmail.com>
2023-06-20 19:17:15 -0300

    drop support for python 3.7 (#131)

--- CCAgT_utils/constants.py
+++ CCAgT_utils/constants.py
@@ -1,11 +1,6 @@
 from __future__ import annotations
 
-import sys
-
-if sys.version_info >= (3, 8):  # pragma: >=3.8 cover
-    import importlib.metadata as importlib_metadata
-else:  # pragma: <3.8 cover
-    import importlib_metadata
+import importlib.metadata as importlib_metadata
 
 
 VERSION = importlib_metadata.version('CCAgT_utils')
